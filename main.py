b0061dcd58b41d29e39233c6f3d125dcbf24bb9f
TheBarnakhil <nsakhil763@gmail.com>
2025-03-12 09:24:02 +0530

    Drew a static triangle on the screen

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import pygame
 
 from constants import *
-
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         for event in pygame.event.get():
@@ -12,7 +18,12 @@ def main():
                 return
 
         screen.fill("black")
+        
+        player.draw(screen)
+
+
         pygame.display.flip()
+        clock.tick(60)
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
