49d9118cc4373dceac941596b84208c5961efe9d
TheBarnakhil <nsakhil763@gmail.com>
2025-03-12 14:02:54 +0530

    Rotate with a and d keys

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ def main():
         screen.fill("black")
         
         player.draw(screen)
+        player.update(dt)
 
 
         pygame.display.flip()
-        clock.tick(60)
+        dt = clock.tick(60) / 100
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
